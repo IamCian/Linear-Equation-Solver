fe9dec0a392f378351bb6f7c5aa9bd05c7b57845
Cian <cianbagshaw@protonmail.com>
2020-01-20 23:37:56 +0000

    Changed input to use keywor arguments

--- LinearEquationSolver.c
+++ LinearEquationSolver.c
@@ -1,59 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main () {
-  printf("Please type the linear sequence with a space in-between each term and then press enter\n");
-  printf("Please enter at least two terms\n");
-  printf("(The program will only use the first 10 terms!)\n\n");
-
-  //Gets all terms in a list
-  char c; //user input
-  short int term = 0; //current term
-  short int minus = 0; //is term a minus
-  short int new = 0; //are they on a new number
-  short int len = 0; //length of the sequence
-  int terms[9]; //terms of the sequence
-  while (len < 10) {
-    c = getchar();
-    if (c>='0' && c<='9') { //if integer
-      putchar(c);
-      term = (term * 10) + (c - '0');
-      new = 1;
-    }
-    if (c == '-') { //if number is minus
-      printf("-");
-      minus = 1;
-    }
-    if (c==' ' && new==1) { //if new number
-      printf(", ");
-      if (minus == 1) {terms[len] = term*-1;}
-      else {terms[len] = term;}
-      term = 0;
-      new = 0;
-      len++;
-    }
-    if (c=='\n') { //if enter pressed
-      if (minus == 1) {terms[len] = term*-1;}
-      else {terms[len] = term;}
-      term = 0;
-      minus = 0;
-      new = 0;
-      len++;
-      break;
-    }
+int main (int argc, char* argv[]) {
+  //Stops if less than two terms are entered
+  if (argc < 3) {
+    printf("You must enter at least TWO terms!\n");
+    return 0;
   }
 
-  //Stops if less than two terms are entered
-  if (len <= 1) {
-    printf("\nYou must enter at least TWO terms!\n");
-    return 0;
+  //Gets all terms in a list
+  short int term = 0; //current term
+  short int i; //index variable for loop
+  int terms[argc-1]; //terms of the sequence
+  for (i = 1; i < argc; i++) {
+    term = atoi(argv[i]); //gets value as integer
+    printf("%d ", term); //print integer
+    terms[i-1] = term; //add to list
   }
 
   //Gets difference
   int diff = terms[1] - terms[0]; //difference between terms
 
   //Stops if it's not linear
-  int i;
-  for (i = 0; i < len-1; i++){
+  for (i = 0; i < argc-2; i++){
     if (terms[i+1] - terms[i] != diff) {
       printf("\nThis is not a linear sequence as the first difference is NOT constant!\n");
       return 0;
--- README.md
+++ README.md
@@ -1,22 +1,15 @@
 # Linear Equation Solver
-This C script is intended to solve linear/arithmetic (sequences with a constatn difference) sequences.
-* It does this by getting the formula for the Nth term of the sequence.
-* It uses the formula: Tn = a + (n - 1)d  
+This C script is intended to solve linear sequences by getting the formula for the Nth term.
+
+# Method
+It gets the Nth term by using the formula: Tn = a + (n - 1)d
 (a=starting term,  d=difference)
-* It supports any positive or negative whole numbers.
 
 # Example Usage
 ```console
-$ cc LinearEquationSolver.c
-$ ./a.out
-Please type the linear sequence with a space in-between each term and then press enter
-Please enter at least two terms
-(The program will only use the first 10 terms!)
-
-13 9 5 1 -3 -7
-13, 9, 5, 1, -3, -7
+$ ./a.out 13 9 5 1 -3 -7
+13 9 5 1 -3 -7 
 d = -4 (difference)
 a = 13 (starting term)
 Tn=17-4n
-$
 ```
